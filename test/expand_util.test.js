65052d86675781b51facb3167e9d7035803e3703
Peranikov <ym.nekoze@gmail.com>
2013-11-12 00:21:02 +0900

    add isJSON test

--- test/expand_util.test.js
+++ test/expand_util.test.js
@@ -5,4 +5,8 @@ describe('isJSON', function() {
   it("JSON形式の文字列が渡された場合、trueを返すべき", function() {
     expect(util.isJSON('{"aaa":"bbb"}')).to.be.ok();
   });
+
+  it("JSON形式以外の文字列が渡された場合、falseを返すべき", function() {
+    expect(util.isJSON('Dummy')).to.not.be.ok();
+  });
 });
